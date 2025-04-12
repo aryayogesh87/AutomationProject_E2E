36288ca41c99a7904aaa2a9096b8bbd76cd1280e
aryayogesh87 <aryayogesh87@gmail.com>
2025-04-12 15:43:21 +0530

    third commit

--- src/test/java/com/orangehrm/automation/tests/SuggestiveDropdowns.java
+++ src/test/java/com/orangehrm/automation/tests/SuggestiveDropdowns.java
@@ -8,20 +8,21 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SuggestiveDropdowns {
-	
+
 	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
 		driver.manage().window().maximize();
-		
+
 		driver.findElement(By.id("autosuggest")).sendKeys("ind");
 		Thread.sleep(3000);
-		List<WebElement>options = driver.findElements(By.xpath("//*[@id='ui-id-1']"));
-		
-		for(WebElement option : options) {
-			if(option.getText().equalsIgnoreCase("India"));
-				option.click();		
-				break;
-			}
-		}	
+		List<WebElement> options = driver.findElements(By.xpath("//*[@id='ui-id-1']"));
+
+		for (WebElement option : options) {
+			if (option.getText().equalsIgnoreCase("India"))
+				;
+			option.click();
+			break;
+		}
 	}
+}
