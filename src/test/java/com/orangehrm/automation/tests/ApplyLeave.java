d2404dfbba6a53f848113a0ce4fbfb8aaa31211c
aryayogesh87 <aryayogesh87@gmail.com>
2025-04-21 14:51:15 +0530

    commit for correcting restructuring

---
+++ src/test/java/com/orangehrm/automation/tests/ApplyLeave.java
@@ -0,0 +1,15 @@
+package com.orangehrm.automation.tests;
+
+import org.openqa.selenium.WebDriver;
+
+import com.automationProjectE2E.base.Base;
+
+public class ApplyLeave extends Base{
+	
+	protected WebDriver driver;
+	driv
+	
+	
+	
+
+}
